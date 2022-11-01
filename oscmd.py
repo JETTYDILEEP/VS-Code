76969c3698c8ae450fed1054dd157a34cc472b21
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2022-11-01 21:18:43 +0530

    first commit

---
+++ oscmd.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('cmd /k "ping 8.8.8.8 & ping 192.168.0.1 -t"')
+
+
---
+++ pywhatsappkit.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+
+'''pywhatkit.sendwhatmsg("+919573645581",
+					"Geeks For Geeks!",
+					22, 22, 15, True, 2)
+'''
+pywhatkit.sendwhatmsg_to_group_instantly("Avengers_Of_India_Officl", "Hey All Please avoid this message just checking!")
